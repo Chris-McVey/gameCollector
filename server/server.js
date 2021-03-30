1428fc8a66aeab7a7a664d5324a0664252dd60e3
Chris McVey <chris.mcvey@gmail.com>
2021-03-30 13:38:03 -0700

    Basic server endpoints set up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express()
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+
+app.get('/api/individual', (req, res) => {
+  console.log('Serving GET request for individual search');
+  const { query } = req.body;
+
+  axios.get(`https://www.pricecharting.com/api/product?t=${process.env.PC_TOKEN}&q=${query}`).then((data) => {
+    res.status(200).send(data.data);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).send(err);
+  })
+})
+
+app.get('/api/group', (req, res) => {
+  console.log('Serving GET request for group search');
+  const { query } = req.body;
+
+  axios.get(`https://www.pricecharting.com/api/products?t=${process.env.PC_TOKEN}&q=${query}`).then((data) => {
+    res.status(200).send(data.data);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).send(err);
+  })
+})
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is up and listening on port ${PORT}`);
